8dc0a84aa85d17fb762945370dd11fdc84f6edd1
NickolasHKraus <NickHKraus@gmail.com>
2022-01-13 15:27:24 -0600

    Infrable.io -> Infrable

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Infrable.io Infrastructure
+# Infrable Infrastructure
 
 [![MIT License](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/infrable-io/infrable-io-infrastructure/blob/master/LICENSE)
-[![Maintained by Infrable.io](https://img.shields.io/badge/Maintained%20by-Infrable.io-000000)](https://infrable.io)
+[![Maintained by Infrable](https://img.shields.io/badge/Maintained%20by-Infrable-000000)](https://infrable.io)
 
-Core AWS infrastructure for Infrable.io
+Core AWS infrastructure for Infrable
 
 ## Chicken and Egg Problem
 
--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # -----------------------------------------------------------------------------
-# DEPLOY CORE AWS INFRASTRUCTURE FOR INFRABLE.IO
+# DEPLOY CORE AWS INFRASTRUCTURE FOR INFRABLE
 # -----------------------------------------------------------------------------
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-infrable-io"
-    key            = "infrable-io-infrastructure/terraform.tfstate"
+    bucket         = "terraform-state-infrable"
+    key            = "infrable-infrastructure/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-infrable-io"
+    dynamodb_table = "terraform-state-infrable"
   }
 }
 
@@ -16,6 +16,6 @@ provider "aws" {
 
 module "terraform_aws_s3_backend" {
   source         = "git@github.com:infrable-io/terraform-aws-s3-backend"
-  s3_bucket      = "terraform-state-infrable-io"
-  dynamodb_table = "terraform-state-infrable-io"
+  s3_bucket      = "terraform-state-infrable"
+  dynamodb_table = "terraform-state-infrable"
 }
